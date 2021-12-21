bf31ed1452cb07645f19cc264178d754d9be34a0
Documenter.jl <documenter@juliadocs.github.io>
2021-12-21 17:18:03 +0000

    build based on a9bc51b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Etcetera","category":"page"},{"location":"#Etcetera","page":"Home","title":"Etcetera","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Etcetera.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Etcetera]","category":"page"}]
+}
